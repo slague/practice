34aaba42b0085daee32ade61b64b30c520724260
slague <stephanielague@gmail.com>
2017-08-21 18:04:44 -0600

    Update practice

--- binary_search.rb
+++ binary_search.rb
@@ -1,5 +1,9 @@
+# This method returns the index of the item in the array
 
 def binary_search(arr, item)
+  arr = arr.sort
+  # The array must be sorted!
+  # see other practice files for other ways to sort the array!
   low = 0
   high = arr.length - 1
 
@@ -21,5 +25,11 @@ end
 
 my_list = [1, 3, 5, 7, 9]
 
+my_unsorted_array = [3, 2, 7, 5, 1, 6, 9, 8]
+
 puts binary_search(my_list, 3)
+puts binary_search(my_list, 9)
 puts binary_search(my_list, 12)
+puts binary_search(my_unsorted_array, 1)
+puts binary_search(my_unsorted_array, 9)
+puts binary_search(my_unsorted_array, 12)
