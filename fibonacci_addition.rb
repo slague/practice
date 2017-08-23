de08da0464c13d734867ba5e686368c11dbaa40a
slague <stephanielague@gmail.com>
2017-08-23 15:22:33 -0600

    Complete fib addition practice

--- fibonacci_addition.rb
+++ fibonacci_addition.rb
@@ -1,1 +1,25 @@
 # By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
+# step 1 find fib items below 4,000,000
+
+def fib_arry
+  arr = [1, 1]
+  a = 0
+  b = 1
+  while arr[a] + arr[b] <= 4000000
+      arr << (arr[a] + arr[b])
+    a += 1
+    b += 1
+  end
+  # puts  arr
+
+  # step 2 determine which are even
+  evens = arr.select { |num| num.even? }
+  # puts evens
+  # step 3 sum
+  puts "This is the sum of all even fibonacci numbers less than 4,000,000:  #{evens.reduce(:+)}"
+
+
+end
+
+fib_arry
