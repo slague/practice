a1eb5e4fd831a682c7ced9b211e6f2587e2b4b3c
slague <stephanielague@gmail.com>
2017-08-28 16:02:47 -0600

    Begin one_to_twenty_wo_remainder

---
+++ one_to_twenty_wo_remainder.rb
@@ -0,0 +1,27 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+#
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+
+def one_to_twenty_wo_remainder
+
+  div_array = Array(1..20)
+  Inf = 1.0/0.0
+  test_smallest = 2520
+
+
+
+
+# go thru div array and divide each num by the test, if every single one is divisible this is it!
+# if, not add one
+#     smallest % div_array.each do |num| == 0
+
+    else
+      a +=1
+    end
+
+  end
+
+end
+
+p one_to_twenty_wo_remainder
