bc5a9a4953a90e40310a4fc87c47f51ccd380f93
slague <stephanielague@gmail.com>
2017-08-28 17:37:44 -0600

    Begin Date night challenge

---
+++ date_night/lib/tree.rb
@@ -0,0 +1,22 @@
+class BinarySearchTree
+
+  def initialize
+    @root_node = nil
+    @total_nodes = 0
+  end
+
+  def insert(value, title)
+
+    if @root_node == nil
+      @root_node = Node.new(value, title)
+
+    else
+       
+    end
+
+  end
+
+end
+
+
+# tree = BinarySearchTree.new
---
+++ date_night/test/tree_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require './lib/tree'
+# require './lib/node'
+require 'minitest/pride'
+
+
+
+class BinarySearchTreeTest < Minitest::Test
+
+  def test_insert
+    
+
+  end
+
+end
