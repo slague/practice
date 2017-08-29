6121b1b4779e46df9fc3f950cf4be1ebae1421fb
slague <stephanielague@gmail.com>
2017-08-29 08:55:27 -0600

    Continue date_night

--- date_night/lib/tree.rb
+++ date_night/lib/tree.rb
@@ -2,29 +2,62 @@ require_relative 'node'
 
 class BinarySearchTree
 
+  attr_accessor :root_node, :values, :total_nodes
   def initialize
     @root_node = nil
     @total_nodes = 0
+    @entered_nodes = {}
   end
 
   def insert(value, title)
     @total_nodes += 1
     if @root_node == nil
       @root_node = Node.new(value, title)
+      @entered_nodes[title] = value
     else
-      @root_node.build(value, title)
+      root_node.build(value, title)
+      @entered_nodes[title] = value
     end
-    # @total_nodes
   end
 
+  def include?(value)
+    @entered_nodes.values.include?(value)
+  end
 
+  def depth_of(value)
+
+  end
+
+  def max
+    max = {}
+    max_val = @entered_nodes.values.max
+    max_title = @entered_nodes.key(max_val)
+    max[max_title] = max_val
+    max
+  end
+
+  def min
+    min = {}
+    min_val = @entered_nodes.values.min
+    min_title = @entered_nodes.key(min_val)
+    min[min_title] = min_val
+    min
+  end
 
 end
 
 
 tree = BinarySearchTree.new
 
-p tree.insert(61, "Bill & Ted's Excellent Adventure")
-p tree.insert(16, "Johnny English")
-p tree.insert(92, "Sharknado 3")
-p tree.insert(50, "Hannibal Buress: Animal Furnace")
+ tree.insert(61, "Bill & Ted's Excellent Adventure")
+ tree.insert(16, "Johnny English")
+ tree.insert(92, "Sharknado 3")
+ tree.insert(50, "Hannibal Buress: Animal Furnace")
+
+p tree.include?(50)
+p tree.include?(26)
+
+# p tree.root_node
+# p tree.values
+p tree.max
+p tree.min
--- date_night/test/tree_test.rb
+++ date_night/test/tree_test.rb
@@ -7,9 +7,21 @@ require 'minitest/pride'
 
 class BinarySearchTreeTest < Minitest::Test
 
-  def test_insert
-
-
+  def skip_test_insert
+    tree = BinarySearchTree.new
+    tree.insert(50, "Jaws")
+    tree.insert(20, "Titanic")
+    tree.insert(80, "Star Wars")
+    assert_equal 1, tree.insert(50, "Jaws")
+    assert_equal 2, tree.insert(20, "Titanic")
+    assert_equal 2, tree.insert(80, "Start Wars")
   end
 
+  def test_include?
+    tree = BinarySearchTree.new
+    tree.insert(50, "Jaws")
+    tree.insert(20, "Titanic")
+    assert tree.include?(50)
+    refute tree.include?(70)
+  end
 end
